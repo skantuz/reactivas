abe333fad6419fdca1e3ce1156b88656c60ad67a
Sergio Lopez <sealopez@bancolombia.com>
2020-03-26 19:54:16 -0500

    gravar monitoriando

--- src/main/java/co/com/bancolombia/reactivas/controller/ReactivasController.java
+++ src/main/java/co/com/bancolombia/reactivas/controller/ReactivasController.java
@@ -3,26 +3,42 @@ package co.com.bancolombia.reactivas.controller;
 import co.com.bancolombia.reactivas.model.Test;
 import co.com.bancolombia.reactivas.model.dto.TestDto;
 import co.com.bancolombia.reactivas.repository.TestRepository;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.*;
 
 import java.time.Duration;
+import java.util.Random;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/test")
 public class ReactivasController {
 
+    final FluxProcessor processor;
+    final FluxSink sink;
+    public ReactivasController() {
+        this.processor = DirectProcessor.create().serialize();
+        this.sink = processor.sink();
+    }
+
     @Autowired
     private TestRepository repository;
+    private static final Logger LOG = LoggerFactory.getLogger(ReactivasController.class);
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     public Mono<Test> save(@RequestBody TestDto testDto){
-        return repository.save(new Test(testDto));
+        return repository.save(new Test(testDto)).map(test -> {
+            sink.next(test);
+            return test;
+        });
     }
 
     @GetMapping(value = "/list")
@@ -30,8 +46,20 @@ public class ReactivasController {
         return repository.findAll();
     }
 
-    @GetMapping(value = "/steam",produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
-    public Flux <Test> steam() throws InterruptedException {
-        return repository.findAll().publish(1).delayElements(Duration.ofSeconds(1));
+    @GetMapping(value = "/create_steam", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Test> steam(){
+        LOG.info("get orientation - HTTP GET CALLED");
+        return Flux.interval(Duration.ofSeconds(1))
+                .flatMap(l -> repository.save(
+                        new Test(null,
+                                RandomStringUtils.random(10),
+                                RandomStringUtils.random(10))))
+                .log();
     }
+
+        @GetMapping(value = "/monitor", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+        public Flux<ServerSentEvent> getSomething() {
+            return processor.map(o -> ServerSentEvent.builder(o).build());
+        }
+
 }
